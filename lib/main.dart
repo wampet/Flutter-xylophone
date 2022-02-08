dea77e3167d5c4b280e6a49b4405abbcb2c210c3
wampet <wampet232@gmail.com>
2022-02-08 15:26:14 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
+// ignore_for_file: missing_required_param, non_constant_identifier_names
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void PlaySound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+//If we want to have named parameters, then we need to enclose the inputs in curly braces
+  buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          PlaySound(soundNumber);
+        },
+        style: TextButton.styleFrom(backgroundColor: color),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
